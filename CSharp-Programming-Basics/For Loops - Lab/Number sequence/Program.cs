b1249285e6fca802b13ebc47dcaf832a9e8a6f29
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:10:39 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/For Loops - Lab/Number sequence/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number >= max)
+                {
+                    max = number;
+                }
+                if (number <= min)
+                {
+                    min = number;
+                }
+            }
+            Console.WriteLine("Max number: {0}", max);
+            Console.WriteLine("Min number: {0}", min);
+        }
+    }
+}
