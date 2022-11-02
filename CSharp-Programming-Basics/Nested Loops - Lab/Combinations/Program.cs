8490e5399989c3da44de3020b43f19b70de2c51d
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:13:47 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/Nested Loops - Lab/Combinations/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int result = 0;
+            int count = 0;
+
+            for (int x1 = 0; x1 <= n; x1++)
+            {
+                for (int x2 = 0; x2 <= n; x2++)
+                {
+                    for (int x3 = 0; x3 <= n; x3++)
+                    {
+                        if (x1 + x2 + x3 == n)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
