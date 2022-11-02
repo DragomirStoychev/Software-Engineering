ba7dc790bb5ec3284b9a067b6eaddc2fafe0e5a6
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:16:37 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/While Loops - Lab/Max Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double max = double.MinValue;
+
+            while (input != "Stop")
+            {
+                double number = Convert.ToDouble(input);
+                if (number > max)
+                {
+                    max = number;
+                }
+                input = Console.ReadLine();
+            }
+            if (input == "Stop")
+            {
+                Console.WriteLine(max);
+            }
+        }
+    }
+}
