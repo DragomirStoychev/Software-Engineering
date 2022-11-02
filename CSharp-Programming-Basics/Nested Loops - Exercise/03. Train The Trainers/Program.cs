61506889d337d208dfa1b5b86ba1420ae62ae00e
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:12:56 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/Nested Loops - Exercise/03. Train The Trainers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            string input = "";
+            double grade = 0;
+            double totalGrade = 0;
+            double totalGrade2 = 0;
+            int count = 0;
+
+            while (input != "Finish")
+            {
+                input = Console.ReadLine();
+                if (input == "Finish")
+                {
+                    Console.WriteLine($"Student's final assessment is {(totalGrade2 / count):f2}.");
+                    return;
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    grade = double.Parse((Console.ReadLine()));
+                    count++;
+                    totalGrade += grade;
+                    totalGrade2 += grade;
+                }
+                Console.WriteLine($"{input} - {(totalGrade / n):f2}.");
+                totalGrade = 0;
+            }
+        }
+    }
+}
