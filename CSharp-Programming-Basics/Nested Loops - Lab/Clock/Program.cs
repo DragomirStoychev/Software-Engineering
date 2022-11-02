0d25c9cbe8665abb175faffb8e477ced872f11c0
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:13:39 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/Nested Loops - Lab/Clock/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int h;
+            int m;
+
+            for (int i = 0; i <= 23; i++)
+            {
+                for (int e = 0; e <= 59; e++)
+                {
+                    Console.WriteLine($"{i}:{e}");
+                }
+            }
+
+        }
+    }
+}
