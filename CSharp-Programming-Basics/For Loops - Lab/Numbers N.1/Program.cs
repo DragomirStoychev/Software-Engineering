a3e2eef00c320f4b802a687e5c3d4d57a1877c18
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:12:01 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/For Loops - Lab/Numbers N.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            for (int i = x; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            } 
+
+        }
+    }
+}
+
+
