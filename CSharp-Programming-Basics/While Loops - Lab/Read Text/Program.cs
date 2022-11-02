aba97e04a019a3fc21aae450f62819fb60c95df1
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:16:56 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/While Loops - Lab/Read Text/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "Stop")
+            {
+                Console.WriteLine(input);
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
