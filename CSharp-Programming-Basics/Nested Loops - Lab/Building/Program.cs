45057c19328d646024df086d03897dbf07e6ef8b
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:13:25 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/Nested Loops - Lab/Building/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main()
+        {
+            int floar = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+
+            int a = 0;
+            int o = 0;
+            int l = 0;
+
+            for (int i = floar; i >= 1 ; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    if (i == floar)
+                    {
+                        for (int y = 0; y < rooms; y++)
+                        {
+                            Console.Write($"L{i}{y} ");
+                        }
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        for (int y = 0; y < rooms; y++)
+                        {
+                            Console.Write($"O{i}{y} ");
+                        }
+                        Console.WriteLine();
+                    }                    
+                }
+                else if (i % 2 != 0)
+                {
+                    if (i == floar)
+                    {
+                        for (int y = 0; y < rooms; y++)
+                        {
+                            Console.Write($"L{i}{y} ");
+                        }
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        for (int x = 0; x < rooms; x++)
+                        {
+                            Console.Write($"A{i}{x} ");
+                        }
+                        Console.WriteLine();
+                    }                        
+                }
+            }
+        }
+    }
+}
