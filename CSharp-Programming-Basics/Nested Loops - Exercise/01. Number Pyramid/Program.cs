e83f8015d8578760c9ac0335ebe8d4664a038526
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:12:38 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/Nested Loops - Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class CubicMessages
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int currentNum = 1;
+
+        int sum = 1;
+        for (int row = 1; row <= n; row++)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write(sum + " ");
+                sum++;
+                if (sum > n)
+                {
+                    return;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
