a150c5ae97607c84014a7c299f628ccddb46a5c6
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:16:02 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/While Loops - Exercise/06. Cake/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double razmeri = a * b;
+
+            string vzemane = "";
+            double vzemaneP = 0;
+
+
+            while (true)
+            {
+                vzemane = Console.ReadLine();
+                if (vzemane == "STOP")
+                {
+                    Console.WriteLine("{0} pieces are left.", razmeri);
+                    break;
+                }
+                vzemaneP = Convert.ToDouble(vzemane);
+                razmeri -= vzemaneP;
+                if (razmeri <= 0)
+                {
+                    Console.WriteLine("No more cake left! You need {0} pieces more.", Math.Abs(razmeri));
+                    break;
+                }
+            }
+        }
+    }
+}
