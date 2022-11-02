c654adc1e8c1c74f981af1077f5b0c1805b651e9
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:16:45 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/While Loops - Lab/Min Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double min = double.MaxValue;
+
+            while (input != "Stop")
+            {
+                double number = Convert.ToDouble(input);
+                if (number < min)
+                {
+                    min = number;
+                }
+                input = Console.ReadLine();
+            }
+            if (input == "Stop")
+            {
+                Console.WriteLine(min);
+            }
+        }
+    }
+}
