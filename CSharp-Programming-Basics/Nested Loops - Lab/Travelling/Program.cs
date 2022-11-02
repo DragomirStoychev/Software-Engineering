67343328970d84faaaedf3b7d457c9eeec8c3259
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:14:28 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/Nested Loops - Lab/Travelling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                string destination = Console.ReadLine();
+                if (destination == "End")
+                {
+                    break;
+                }
+                double sum = 0;
+                double budget = double.Parse(Console.ReadLine());
+                while (budget > sum)
+                {
+                    double sumInput = double.Parse(Console.ReadLine());
+                    sum += sumInput;
+                }
+                Console.WriteLine($"Going to {destination}!");
+            }
+        }
+    }
+}
