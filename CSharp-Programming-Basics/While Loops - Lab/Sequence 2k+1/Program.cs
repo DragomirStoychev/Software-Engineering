0d5c3be81fb8f6d484d7539ab733c2046d27aab2
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:17:07 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/While Loops - Lab/Sequence 2k+1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = 1;
+            int result = n;
+
+            while (n >= result)
+            {
+                Console.WriteLine(k);
+                k = k * 2 + 1;
+                result = k;
+            }
+        }
+    }
+}
