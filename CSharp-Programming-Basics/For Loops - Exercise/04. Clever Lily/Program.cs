f331a6c93860d2b0e2f47598733d8957961a969b
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:07:12 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/For Loops - Exercise/04. Clever Lily/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            decimal priceLoundry = decimal.Parse(Console.ReadLine());
+            decimal priceToy = decimal.Parse(Console.ReadLine());
+
+            decimal birthDayMoney = 10;
+            decimal savedMoney = 0;
+
+            decimal toys = 0;
+
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    savedMoney += birthDayMoney;
+                    savedMoney--;
+                    birthDayMoney += 10;
+                }
+                else
+                {
+                    toys++;
+                }
+            }
+            decimal total = savedMoney + (toys * priceToy);
+
+            if (total >= priceLoundry)
+            {
+                Console.WriteLine("Yes! {0:f2}", total - priceLoundry);
+            }
+            else
+            {
+                Console.WriteLine("No! {0:f2}", Math.Abs(priceLoundry - total));
+            }
+        }
+    }
+}
