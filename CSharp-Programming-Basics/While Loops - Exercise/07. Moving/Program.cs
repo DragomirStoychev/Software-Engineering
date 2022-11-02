1a8aec6d8603690ccd0fff734a3bc5e9f2cc8dde
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:16:09 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/While Loops - Exercise/07. Moving/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int freeSpace = a * b * c;
+
+            string input = "";
+            int box = 0;
+
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "Done")
+                {
+
+                    Console.WriteLine("{0} Cubic meters left.", Math.Abs(freeSpace));
+                    break;
+                }
+                box = Convert.ToInt32(input);
+                freeSpace -= box;
+                if (freeSpace <= 0)
+                {
+
+                    Console.WriteLine("No more free space! You need {0} Cubic meters more.", Math.Abs(freeSpace));
+                    break;
+                }
+            }
+        }
+    }
+}
