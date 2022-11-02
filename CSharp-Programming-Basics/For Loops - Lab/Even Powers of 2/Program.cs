2dae0965dc2c1ba04702d6b4f79f2e6457079803
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:08:54 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/For Loops - Lab/Even Powers of 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double number = 2;
+
+
+            for (int i = 0; i < n+1; i++)
+            {
+                number = Math.Pow(2, i);
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                }                
+            }
+        }
+    }
+}
