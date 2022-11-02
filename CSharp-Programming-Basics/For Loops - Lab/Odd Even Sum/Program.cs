7c129e59465f464af64b49e7cbcc7f1c657a87fd
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:12:09 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/For Loops - Lab/Odd Even Sum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int odd = 0;
+            int even = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    even += number;
+                }
+                else
+                {
+                    odd += number;
+                }                
+            }
+
+            if (even == odd)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = {0}", odd);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = {0}", Math.Abs(odd - even));
+            }
+        }
+    }
+}
