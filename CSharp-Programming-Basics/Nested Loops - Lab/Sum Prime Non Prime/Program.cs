7d03fd4259bdd17a7280f5483bea36d9fe112c5c
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:14:20 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/Nested Loops - Lab/Sum Prime Non Prime/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            int prime = 0;
+            int nonPrime = 0;
+            
+
+            while (input != "stop")
+            {
+                input = Console.ReadLine();
+                bool isNumberPrime = true;
+                if (input == "stop")
+                {
+                    Console.WriteLine($"Sum of all prime numbers is: {prime}");
+                    Console.WriteLine($"Sum of all non prime numbers is: {nonPrime}");
+                    return;
+                }
+                int number = Convert.ToInt32(input);
+                if (number < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                    continue;
+                }
+                for (int i = 2; i < number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        isNumberPrime = false;
+                        break;
+                    }
+                }
+                if (isNumberPrime)
+                {
+                    prime += number;
+                }
+                else
+                {
+                    nonPrime += number;
+                }
+            }
+        }
+    }
+}
