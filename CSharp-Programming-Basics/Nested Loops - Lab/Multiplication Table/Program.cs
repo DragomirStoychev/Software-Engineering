27e813193a44da050dd01a50bc183c7e9f1a4b7a
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:13:54 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/Nested Loops - Lab/Multiplication Table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int e = 1; e <= 10; e++)
+                {
+                    int x = i * e;
+                    Console.WriteLine($"{i} * {e} = {x}");
+                }
+
+            }
+
+        }
+    }
+}
