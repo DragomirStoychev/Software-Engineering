af10cc252befb4471cab1c853ad1673ef7d81b02
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:16:52 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/While Loops - Lab/Password/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            string input = Console.ReadLine();
+
+            while (input != password)
+            {
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Welcome {name}!");
+        }
+    }
+}
