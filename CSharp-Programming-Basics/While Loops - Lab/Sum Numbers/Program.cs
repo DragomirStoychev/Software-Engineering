be9fc1a0eab098e57fc61e5e4d664f003114d997
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:17:14 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/While Loops - Lab/Sum Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int input = 0;
+
+            while (sum < number)
+            {                              
+                input = int.Parse(Console.ReadLine());
+                sum += input;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
