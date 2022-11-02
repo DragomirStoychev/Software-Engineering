9822404885efb75bded5a0252b006019ae097a63
Dragomir Stoychev <dragomirmd@gmail.com>
2022-11-02 14:06:44 +0200

    Adding resources from C# Basics

---
+++ CSharp-Programming-Basics/For Loops - Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int max = int.MinValue;
+            int sum = 0;
+            int total = sum - max;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number > max)
+                {
+                    max = number;
+                }
+                sum += number;
+            }
+
+            if (max == sum-max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = {0}", max);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = {0}", Math.Abs(max - (sum-max)));
+            }
+        }
+    }
+}
